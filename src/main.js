b0ec497913d37abaa23a72b7e4721a77da1f3b12
umv23 <umv23@cs.drexel.edu>
2020-04-13 14:46:38 -0400

    updating app with firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/assets/style.scss";
+import { fireAuth } from "@/db/firebase";
 
 import jQuery from "jquery";
 import "popper.js";
@@ -12,8 +13,13 @@ window.$ = window.jQuery = jQuery;
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+let app;
+fireAuth.onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
